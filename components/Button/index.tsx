5e02d850e3495c08ed96f42ad4917d988f4087a8
[Darrick Fauvel] <[darrick@darrickdevelops.com]>
2024-06-20 18:54:14 -0400

    Make button wider

--- components/Button/index.tsx
+++ components/Button/index.tsx
@@ -7,7 +7,7 @@ type ButtonProps = {
 
 export default function RollButton(props: ButtonProps) {
   return (
-    <Button className="mt-6 w-1/2 py-6" onClick={props.onClick}>
+    <Button className="mt-6 w-3/4 py-6" onClick={props.onClick}>
       Roll Dice
     </Button>
   )

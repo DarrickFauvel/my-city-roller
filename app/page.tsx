93c490311163c969212dd71007f78e1fefd7770a
[Darrick Fauvel] <[darrick@darrickdevelops.com]>
2024-06-19 20:19:13 -0400

    Remove commented out code

--- app/page.tsx
+++ app/page.tsx
@@ -7,53 +7,6 @@ export default function Home() {
       <h1>My City Roller</h1>
 
       <DiceDisplay />
-
-      {/* <span>corner</span>
-      <div className="die building-shape-outer">
-        <div className="building-shape-inner shape-corner">
-          <span className="block"></span>
-          <span className="block"></span>
-          <span className="block"></span>
-          <span className="block"></span>
-          <span className="circle right"></span>
-        </div>
-      </div>
-
-      <span>one horizontal</span>
-      <div className="die building-shape-outer">
-        <div className="building-shape-inner shape-one-horizontal">
-          <span className="block"></span>
-          <span className="block"></span>
-          <span className="block"></span>
-          <span className="block"></span>
-          <span className="circle right"></span>
-        </div>
-      </div>
-
-      <span>two horizontal</span>
-      <div className="die building-shape-outer">
-        <div className="building-shape-inner shape-two-horizontal">
-          <span className="block"></span>
-          <span className="block"></span>
-          <span className="block"></span>
-          <span className="block"></span>
-          <span className="circle right"></span>
-        </div>
-      </div>
-
-      <span>two vertical</span>
-      <div className="die building-shape-outer">
-        <div className="building-shape-inner shape-two-vertical">
-          <span className="block"></span>
-          <span className="block"></span>
-          <span className="block"></span>
-          <span className="block"></span>
-          <span className="circle right"></span>
-        </div>
-      </div>
-
-      <h2>Building type</h2>
-      <div className="die builing-type"></div> */}
     </main>
   )
 }

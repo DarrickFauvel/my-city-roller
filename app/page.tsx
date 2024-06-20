2205804e60605a888e62c113471ba5471516a074
[Darrick Fauvel] <[darrick@darrickdevelops.com]>
2024-06-20 07:31:35 -0400

    Lift diceRolled state

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,10 @@
 import "@/assets/css/dice.css"
 import DiceDisplay from "@/components/DiceDisplay"
 import Header from "@/components/Header"
+import { useState } from "react"
 
 export default function Home() {
+  const [diceRolled, setDiceRolled] = useState(false)
   return (
     <main className="prose">
       <Header />

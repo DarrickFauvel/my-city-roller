58c8dc10114dda69437726303ae62054204a043a
[Darrick Fauvel] <[darrick@darrickdevelops.com]>
2024-06-20 09:00:15 -0400

    Adjust colors

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -14,29 +14,30 @@ export default function Header(props: HeaderProps) {
   const { showDiceRolled } = props
 
   return (
-    <header className="flex items-center justify-between bg-blue-200 px-8">
+    <header
+      className={`flex items-center justify-between bg-gradient-to-b from-cyan-700 to-cyan-200 px-8`}>
       <span
         className={`${cormorant.className} text-white text-4xl font-semibold sm:text-4xl hover:cursor-pointer animate-fade-in`}
         onClick={() => showDiceRolled(false)}>
         My City Roller
       </span>
-      <div className="flex gap-3">
+      <div className="flex gap-2">
         <Image
-          className="shadow-lg -rotate-12"
+          className="w-6 sm:w-12 outline outline-1 outline-cyan-500 shadow-cyan-300 -rotate-12 animate-roll-in-blurred-right transition delay-1000"
           src={BlueDieLeft}
           width={30}
           height={30}
           alt="blue building shape die #1"
         />
         <Image
-          className="shadow-lg rotate-3"
+          className="w-6 sm:w-12 outline outline-1 outline-cyan-500 shadow-cyan-300 rotate-3 animate-roll-in-blurred-right transition delay-1500"
           src={BlueDieRight}
           width={30}
           height={30}
           alt="blue building shape die #2"
         />
         <Image
-          className="shadow-lg -rotate-45 scale-110"
+          className="w-6 sm:w-12 outline outline-1 outline-cyan-500 shadow-cyan-300 -rotate-45 scale-110 animate-roll-in-blurred-right transition delay-2000"
           src={WhiteDie}
           width={30}
           height={30}
